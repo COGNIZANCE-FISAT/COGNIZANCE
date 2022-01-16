c5533846a4711e96f4c4f3fbf0b05dbdf4fe6166
Josephine James <josephinejames365@gmail.com>
2022-01-16 23:53:56 +0530

    homepage background

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import "./Home.css";
+import React from "react";
+import { bg } from "./image2vector.svg";
+const Home =() =>{
+    return (
+        <div className="homepage-image" id="/home">
+    
+        </div>
+    )
+}
+export default Home;
