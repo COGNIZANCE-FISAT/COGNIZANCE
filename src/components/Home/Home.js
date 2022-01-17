3f6e3815913d5d0624cbc21b02038c86acc955c0
Josephine James <josephinejames365@gmail.com>
2022-01-17 18:11:30 +0530

    Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,22 @@
 import "./Home.css";
 import React from "react";
-import { bg } from "./image2vector.svg";
+import VideoPlayer from 'react-video-js-player';
+import fisat from './fisat.png';
+import COG from "./Cog.mp4";
+
 const Home =() =>{
+    const videoSrc = COG;
+    const poster = fisat;
     return (
-        <div className="homepage-image" id="/home">
-    
-        </div>
+        <>
+        <div className="homepage-image">
+        <br/>
+        
+    </div>
+    <center>
+    <VideoPlayer src={videoSrc} poster ={poster} width="980" height="600" autoplay/>
+    </center>
+    </>
     )
 }
 export default Home;
